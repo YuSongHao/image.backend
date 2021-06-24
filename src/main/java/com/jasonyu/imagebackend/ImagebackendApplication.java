3af795a9dafcc91a68b30857ad354ea3cec0c24e
jasonyu <jasonysh419@gmail.com>
2021-06-24 22:44:48 +0800

    project init

---
+++ src/main/java/com/jasonyu/imagebackend/ImagebackendApplication.java
@@ -0,0 +1,13 @@
+package com.jasonyu.imagebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagebackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImagebackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jasonyu/imagebackend/ImagebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasonyu.imagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagebackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
